ae7471847e25c2e46441ed421dfb1402e8fb8506
Your Name <you@example.com>
2026-02-27 23:51:10 +0530

    first commit

--- src/Solution.java
+++ src/Solution.java
@@ -5,30 +5,21 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Read a single line of text
-        String line = scanner.nextLine();
-        
-        // Split the text into words
-        String[] words = line.split(" ");
-        
-        // Create a HashMap
+
+        String line = scanner.nextLine().trim();
+
+        String[] words = line.split("\\s+");
+
         HashMap<String, Integer> frequencyMap = new HashMap<>();
-        
-        // Count word frequencies
+
         for (String word : words) {
-            if (frequencyMap.containsKey(word)) {
-                frequencyMap.put(word, frequencyMap.get(word) + 1);
-            } else {
-                frequencyMap.put(word, 1);
-            }
+            frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
         }
-        
-        // Print word: count
+
         for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
-        
+
         scanner.close();
     }
 }
