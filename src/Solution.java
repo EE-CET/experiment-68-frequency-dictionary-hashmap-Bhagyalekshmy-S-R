281b136b8e059211998a7c3c4bc8405a9bb8880a
Your Name <you@example.com>
2026-02-28 00:15:47 +0530

    first commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,6 @@
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Solution {
     public static void main(String[] args) {
@@ -9,7 +9,7 @@ public class Solution {
         String line = scanner.nextLine().trim();
         String[] words = line.split("\\s+");
 
-        HashMap<String, Integer> map = new HashMap<>();
+        TreeMap<String, Integer> map = new TreeMap<>();
 
         for (String word : words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
@@ -18,7 +18,7 @@ public class Solution {
         boolean first = true;
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
             if (!first) {
-                System.out.print("\n");
+                System.out.println();
             }
             System.out.print(entry.getKey() + ": " + entry.getValue());
             first = false;
